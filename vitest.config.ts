1d46a12201059180e6282a6e9ebda11c8dad74c7
Carlos Precioso <carlos@wasp-lang.dev>
2026-02-05 19:19:24 +0100

    Fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config";
 
-defineConfig({
+export default defineConfig({
   test: {
     testTimeout: 30_000, // 30 seconds, which is needed for the installer tests that involve downloading and installing Wasp.
   },
